4cd9ce9d3926fe5be780f6daaa3b8e5dd69d9143
joell-oss <jozef.sroka.26@gmail.com>
2025-06-14 13:20:38 +0200

    Update keras2onnx.py

--- scripts/keras2onnx.py
+++ scripts/keras2onnx.py
@@ -3,6 +3,20 @@
 Spyder Editor
 Józef Sroka, 67195-CKP, 2025, 67195-ckp@kozminski.edu.pl
 """
+
+"""
+Konwersja modelu Keras do formatu ONNX dla zwiêkszenia przenoœnoœci i kompatybilnoœci.
+Skrypt wczytuje wczeœniej wytrenowany model sieci neuronowej zapisany w formacie Keras
+i przekszta³ca go do standardu ONNX (Open Neural Network Exchange). Format ONNX umo¿liwia
+uruchamianie modelu w ró¿nych œrodowiskach i platformach, niezale¿nie od frameworka
+u¿ytego do trenowania. Konwersja zachowuje architekturê sieci oraz wagi modelu.
+Proces obejmuje:
+- Wczytanie modelu Keras z pliku kontrolnego
+- Okreœlenie specyfikacji wejœciowej na podstawie wymiarów modelu
+- Konwersjê do formatu ONNX z wykorzystaniem operatorów w wersji 13
+- Zapisanie skonwertowanego modelu do pliku binarnego
+"""
+
 import tensorflow as tf
 import tf2onnx
 
